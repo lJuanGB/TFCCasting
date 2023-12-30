c9db0b5a8da8f5f3ca2505642f86a4302da5d5b1
pietro-lopes <unknownrj@gmail.com>
2023-12-30 03:20:53 -0300

    Updating Creative Tabs Registry
    - onTextureStitch doesn't exist, now you add texture using json at assets/atlases
    - added Items and BlockItems to TFC Creative tab, making sure food is not Rotten

--- src/main/java/com/ljuangbminecraft/tfcchannelcasting/client/ClientEventHandler.java
+++ src/main/java/com/ljuangbminecraft/tfcchannelcasting/client/ClientEventHandler.java
@@ -2,12 +2,14 @@ package com.ljuangbminecraft.tfcchannelcasting.client;
 
 import com.ljuangbminecraft.tfcchannelcasting.common.blockentities.TFCCCBlockEntities;
 
-import net.dries007.tfc.client.RenderHelpers;
-import net.minecraft.resources.ResourceLocation;
+import com.ljuangbminecraft.tfcchannelcasting.common.blocks.TFCCCBlocks;
+import com.ljuangbminecraft.tfcchannelcasting.common.items.TFCCCItems;
+import net.dries007.tfc.common.capabilities.food.FoodCapability;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.dries007.tfc.common.TFCCreativeTabs;
 
 public final class ClientEventHandler
 {
@@ -16,7 +18,7 @@ public final class ClientEventHandler
         final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         bus.addListener(ClientEventHandler::registerEntityRenderers);
-        bus.addListener(ClientEventHandler::onTextureStitch);
+        bus.addListener(ClientEventHandler::addItemsToCreativeTab);
     }
 
     public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event)
@@ -25,14 +27,19 @@ public final class ClientEventHandler
         event.registerBlockEntityRenderer(TFCCCBlockEntities.MOLD_TABLE.get(), ctx -> new MoldBlockEntityRenderer());
     }
 
-    public static void onTextureStitch(TextureStitchEvent.Pre event)
+    public static void addItemsToCreativeTab(BuildCreativeModeTabContentsEvent event)
     {
-        final ResourceLocation sheet = event.getAtlas().location();
-        if (sheet.equals(RenderHelpers.BLOCKS_ATLAS))
-        {
-            event.addSprite(new ResourceLocation("tfcchannelcasting", "block/metal/full/dark_chocolate") );
-            event.addSprite(new ResourceLocation("tfcchannelcasting", "block/metal/full/milk_chocolate") );
-            event.addSprite(new ResourceLocation("tfcchannelcasting", "block/metal/full/white_chocolate") );
+        if (event.getTab() == TFCCreativeTabs.FOOD.tab().get()){
+            event.acceptAll(TFCCCItems.CHOCOLATE_SWEET.values().stream().flatMap(map -> map.values().stream().map(reg -> FoodCapability.setStackNonDecaying(reg.get().getDefaultInstance()))).toList());
+        }
+        if (event.getTab() == TFCCreativeTabs.MISC.tab().get()){
+            event.accept(TFCCCItems.UNFIRED_CHANNEL);
+            event.accept(TFCCCItems.UNFIRED_MOLD_TABLE);
+            event.accept(TFCCCItems.UNFIRED_HEART_MOLD);
+            event.accept(TFCCCItems.HEART_MOLD);
+            // event.acceptAll(TFCCCItems.EXTRA_FLUID_BUCKETS.values().stream().map(reg -> reg.get().getDefaultInstance()).toList());
+            event.accept(TFCCCBlocks.CHANNEL);
+            event.accept(TFCCCBlocks.MOLD_TABLE);
         }
     }
 }
